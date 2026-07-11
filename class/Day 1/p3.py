4068746b13bf2c9a8440cf67bbb8b9b874f31842
g24.abdulrazak.shaikh@gnkhalsa.edu.in <g24.abdulrazak.shaikh@gnkhalsa.edu.in>
2026-07-11 10:09:59 +0530

    Immutability

--- class/Day 1/p3.py
+++ class/Day 1/p3.py
@@ -1,28 +1,19 @@
-# Dunder methods : Double Underscore
-class Book:
-    def __init__(self,title,author):
-        self.title = title
-        self.author = author
-    # String representation for users
-    def __str__(self):
-        return f"{self.title} by {self.author}"
-    # String representation for developers
-    def __repr__(self):
-        return f"Book('{self.title}','{self.author}')"
+# Immutability
 
-    # Equality comparison
-    def __eq__(self,other):
-        if not isinstance(other,Book):
-            return False
-        return self.title == other.title and self.author == other.author
-    
-book1 = Book("Python 101", "Mark Davis")
-book2 = Book("Python 101", "Mark Davis")
-book3 = Book("Advanced Python", "Tom Harry")
+# Mutable
+my_list = [1,2,3]
+my_list[0] = 10
+# print(my_list)
 
-# print(book1)
-print(repr(book1))
-# print(book1 == book2)
-# print(book1 == book3)
-# __getitem__ : indexing access
-# __call__ : make object callable
+# Immutable
+my_tuple = (1,2,3)
+# my_tuple[0] = 10
+
+def pure_process(data):
+    return data + (4,5,6)
+
+original = (1,2,3)
+processed = pure_process(original)
+
+print(original)
+print(processed)
